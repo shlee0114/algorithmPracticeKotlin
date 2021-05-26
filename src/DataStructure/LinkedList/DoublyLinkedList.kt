645acbbed61026bbc4fdaa2370e0b785eb27187a
lsh <tlgj255@naver.com>
2021-05-26 23:16:14 +0900

    Fix : DoublyLinkedList를 실 사용할 수 있게 변경 operator추가 및 operator사용을 위해 class로 변경

--- src/DataStructure/LinkedList/DoublyLinkedList.kt
+++ src/DataStructure/LinkedList/DoublyLinkedList.kt
@@ -2,8 +2,21 @@ package DataStructure.LinkedList
 
 
 //이중 연결 리스트
-object DoublyLinkedList {
-    var nextNode : DoublyLinkedList? = null
-    var prevNode : DoublyLinkedList? = null
-    lateinit var value : Any
+class DoublyLinkedList(var nextNode : DoublyLinkedList? = null, var prevNode : DoublyLinkedList? = null, var value : Int = -1) {
+
+    operator fun plus(i: Int): Int {
+        return value + i
+    }
+
+    operator fun plusAssign(i : Int) {
+        value += i
+    }
+
+    operator fun compareTo(i: Int) : Int =
+        when{
+            value > i -> 1
+            value < i -> -1
+            else -> 0
+        }
+
 }
