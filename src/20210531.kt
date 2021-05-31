9db7b81ff2987b9c2507a814c6cf5b01fc49b858
lsh <tlgj255@naver.com>
2021-05-31 22:39:04 +0900

    Fix : Stack으로 푼 문제가 정상적인 답이 안나오는 에러 수정
    변경사항 : queue와 동일하게 스택을 pop한 후 값이 가장 큰 값일 경우 저장하지 않고 answer값을 증가시킨 후 기존에는 tmp의 크기만 비교했지만 현재 걸린 값을 제외하고 모든 값의 크기로 가장 큰 값을 추출하도록 수정

--- src/20210531.kt
+++ src/20210531.kt
@@ -23,20 +23,19 @@ class Fir_20210531 {
         this.maxVal = priorities.maxOrNull() ?: 0
         //첫 한 바퀴는 받은 배열만큼 돌면서 만약 최대 값이면 바로 pop을 해준다.
         for(i in priorities.indices){
-            if(insertValueStack(stack, intArrayOf(priorities[i], i))){
-                return answer
-            }
+            stack.push(intArrayOf(priorities[i], i))
         }
 
         //첫 한 바퀴 이후는 기존에 저장한 스택의 길이만큼 반복하면서 임시 스택에 위에 했던 것과 동일하게 넣으면서 최대 값과 비교한다.
+        var tmpStack = CustomStack()
         while (true){
-            val tmpStack = CustomStack()
-            for(i in stack){
-                if(insertValueStack(tmpStack, i.value as IntArray)){
-                    return answer
-                }
+            if(insertValueStack(tmpStack, stack)){
+                return answer
             }
-            stack = tmpStack
+            if(stack.isEmpty()){
+                stack = tmpStack
+                tmpStack = CustomStack()
+            }
         }
         return answer
     }
@@ -44,23 +43,32 @@ class Fir_20210531 {
     //넣는 데이터가 최대 값인지 확인 후 데이터를 넣고 만약 최대 값이고 사용자가 원하는 위치라면 true로 반환
     //stack : 데이터를 넣는 스택
     //insertValue : 스택에 넣는 데이터 배열이며 0에는 값 1에는 위치가 저장되어있다
-    private fun insertValueStack(stack : CustomStack, insertValue : IntArray) : Boolean{
-        if(insertValue[0] >= this.maxVal) {
-            answer++
-            if(insertValue[1] == location){
-                return true
+    private fun insertValueStack(tmpStack : CustomStack, stack : CustomStack) : Boolean{
+        stack.pop()?.let {
+            if((it as IntArray)[0] >= this.maxVal) {
+                answer++
+                if(it[1] == location){
+                    return true
+                }
+                getMaxVal(tmpStack, stack)
+            }else{
+                tmpStack.push(it)
             }
-            getMaxVal(stack)
-        }else{
-            stack.push(insertValue)
         }
         return false
     }
 
     //스택을 전부 돌면서 해당 스택 내에 있는 값들의 최대 값을 구하는 작업
     //stack : 최대 값을 구할 스택
-    private fun getMaxVal(stack: CustomStack){
+    private fun getMaxVal(tmpStack : CustomStack,stack: CustomStack){
         this.maxVal = 0
+        loopStackGetMax(tmpStack)
+        loopStackGetMax(stack)
+    }
+
+    //같은 식이 두 번이나 반복되어서 하나의 메소드로 구현
+    //queue : 최대 값을 구할 큐
+    private fun loopStackGetMax(stack:  CustomStack){
         if(stack.isNotEmpty()) {
             for (j in stack) {
                 if (this.maxVal < (j.value as IntArray)[0]) {
@@ -139,9 +147,9 @@ class Snd_20210531{
 fun main(){
     val fir = Fir_20210531()
     val snd = Snd_20210531()
-    println(snd.solution(intArrayOf(2, 1, 3, 2), 2))
-    println(snd.solution(intArrayOf(2, 1, 3, 2), 2))
-    println(snd.solution(intArrayOf(9, 3, 1, 11), 2))
-    println(snd.solution(intArrayOf(24,51, 13, 25, 43, 12, 43, 11, 32), 2))
-    println(snd.solution(intArrayOf(0, 0, 0, 0), 2))
+    println(fir.solution(intArrayOf(2, 1, 3, 2), 2))
+    println(fir.solution(intArrayOf(2, 1, 3, 2), 2))
+    println(fir.solution(intArrayOf(9, 3, 1, 11), 2))
+    println(fir.solution(intArrayOf(24,51, 13, 25, 43, 12, 43, 11, 32), 2))
+    println(fir.solution(intArrayOf(0, 0, 0, 0), 2))
 }
