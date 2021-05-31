3b4d5eb08d8d4229991bbcf77ea074603761a4ff
tlgj255 <tlgj255@naver.com>
2021-05-31 18:07:39 +0900

    Refactor : 입력 변수 변경
    IntArray를 배열로 저장해야해서 Int -> Any타입으로 변경

--- src/DataStructure/LinkedList/DoublyLinkedList.kt
+++ src/DataStructure/LinkedList/DoublyLinkedList.kt
@@ -2,21 +2,4 @@ package DataStructure.LinkedList
 
 
 //이중 연결 리스트
-class DoublyLinkedList(var nextNode : DoublyLinkedList? = null, var prevNode : DoublyLinkedList? = null, var value : Int = -1) {
-
-    operator fun plus(i: Int): Int {
-        return value + i
-    }
-
-    operator fun plusAssign(i : Int) {
-        value += i
-    }
-
-    operator fun compareTo(i: Int) : Int =
-        when{
-            value > i -> 1
-            value < i -> -1
-            else -> 0
-        }
-
-}
+class DoublyLinkedList(var nextNode : DoublyLinkedList? = null, var prevNode : DoublyLinkedList? = null, var value : Any? = null)
