ab0aeeed21f6e3f4d83b0c7f6074e4ad4ea45a50
tlgj255 <tlgj255@naver.com>
2021-07-27 18:15:38 +0900

    Add : 프로그래머스 문제의 예시를 코틀린 배열에 맞게 글자 변환 모듈 개발

---
+++ src/FunctionModules/ProgrammersExampleArrayToKotlinArray.kt
@@ -0,0 +1,12 @@
+package FunctionModules
+
+class ProgrammersExampleArrayToKotlinArray {
+    fun toArrayText(text : String) = text.replace("[", "arrayOf(").replace("]", ")")
+
+}
+
+fun main(){
+    val text = ProgrammersExampleArrayToKotlinArray()
+    print(text.toArrayText("[[\"100\",\"ryan\",\"music\",\"2\"],[\"200\",\"apeach\",\"math\",\"2\"],[\"300\",\"tube\",\"computer\",\"3\"],[\"400\",\"con\",\"computer\",\"4\"],[\"500\",\"muzi\",\"music\",\"3\"],[\"600\",\"apeach\",\"music\",\"2\"]]"))
+
+}
